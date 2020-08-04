6658c36279c491e4406cd8f24b0493721c6078a5
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-08-04 11:17:01 +0200

    Revise article's content

--- src/main/java/com/hazelcast/springboot/caching/BookController.java
+++ src/main/java/com/hazelcast/springboot/caching/BookController.java
@@ -1,6 +1,5 @@
 package com.hazelcast.springboot.caching;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/books")
 public class BookController {
-    @Autowired
-    private BookService bookService;
+
+    private final BookService bookService;
+
+    BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     @GetMapping("/{isbn}")
     public String getBookNameByIsbn(@PathVariable("isbn") String isbn) {
